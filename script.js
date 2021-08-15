28926ccc147c1935ef0e87ea2b203824ce809e88
shubham <shubhambishnoi78@gmail.com>
2021-08-15 21:00:32 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const birthDate = document.querySelector("#dob");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector(".result");
+
+checkButton.addEventListener("click", function checkBirthDay(){
+
+    var bDay = birthDate.value;
+    bDay = bDay.replaceAll("-","")
+    
+    let sum = 0
+
+    if(bDay && luckyNumber.value){
+
+        for(let i=0;i<bDay.length;i++){
+            sum = sum + Number(bDay.charAt(i));
+        }
+        message.style.display = "none";
+        if(sum % luckyNumber.value === 0){
+            message.style.display="block";
+            message.style.color="blue";
+            message.innerHTML = "Hurray !!! Your Birthday is Lucky"
+        }else{
+            message.style.display="block";
+            message.style.color="blue";
+            message.innerHTML = "Oh no!!! Your Birthday is Not Lucky"
+        }
+
+
+    }else {
+        message.style.color="red";
+        message.innerText = "Please enter both inputs"
+    }
+})
